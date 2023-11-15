b3f77efc26775923f92e9229f0632e5238e7cbc5
calis7 <camilotero89@gmail.com>
2023-11-15 11:13:58 +0100

    Commit inicial

---
+++ script.js
@@ -0,0 +1,15 @@
+// Obtén una NodeList de todos los elementos con la clase "arrow"
+const arrows = document.querySelectorAll('.arrow');
+
+// Itera sobre cada flecha y agrega un evento de clic
+arrows.forEach(arrow => {
+  arrow.addEventListener('click', function() {
+    const subMenu = this.parentElement.nextElementSibling; // El submenú asociado a la flecha
+
+    // Cambia la visibilidad del submenú
+    subMenu.classList.toggle('show');
+
+    // Cambia la clase "expanded" para girar la flecha
+    this.parentElement.classList.toggle('expanded');
+  });
+});
